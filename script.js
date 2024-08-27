f8d8a963ee85b95633e76db228863beb84e5af77
Niloufar Shahdoust <niloufar.shahdoust@utah.edu>
2024-08-27 13:50:30 -0600

    updates

--- script.js
+++ script.js
@@ -3,31 +3,38 @@ import { runTask } from './task.js';
 import { runTaskMissed } from './task_missed.js';
 import { taskDescription } from './task_description.js';
 
+// Dataset name trial
+var datasetNameTrial = {
+  type: jsPsychSurveyText,
+  questions: [
+    { prompt: "Please enter the name of the dataset:" }
+  ],
+  on_finish: function(data){
+    var datasetName = data.response.Q0; // Save the dataset name
+    jsPsych.data.addProperties({ datasetName: datasetName });
+  }
+};
+
 // Initialize jsPsych
-const jsPsych = initJsPsych();
+const jsPsych = initJsPsych({
+  on_finish: function() {
+    var csvData = jsPsych.data.get().csv();
+    var datasetName = jsPsych.data.get().last(1).values()[0].datasetName;
 
-// Fixed trials with uni first, high next, low last
-const trialNumber_fixed_uni = [];
-for (let temp_var = 0 * eachClassTrialNumber; temp_var < 1 * eachClassTrialNumber; temp_var++) {
-  trialNumber_fixed_uni.push(temp_var);
-}
+    // Define the filename with your desired path
+    var fileName = "data/" + datasetName + ".csv"; // Suggesting "data" directory
 
-const trialNumber_fixed_low_first = [];
-for (let temp_var = 1 * eachClassTrialNumber; temp_var < 2 * eachClassTrialNumber; temp_var++) {
-  trialNumber_fixed_low_first.push(temp_var); 
-}
-
-const trialNumber_fixed_high_first = [];
-for (let temp_var = 2 * eachClassTrialNumber; temp_var < 3 * eachClassTrialNumber; temp_var++) {
-  trialNumber_fixed_high_first.push(temp_var);
-}
-
-// Mixed trials for the second round
-let trialNumber_mixed = [];
-for (let i = 0; i < NumberOfTrials; i++) {
-  trialNumber_mixed.push(i);
-}
-trialNumber_mixed = jsPsych.randomization.shuffle(trialNumber_mixed);
+    var blob = new Blob([csvData], { type: 'text/csv' });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = fileName;  // This is the suggested filename
+    document.body.appendChild(a);
+    a.click();
+    URL.revokeObjectURL(url);
+  }
+});
 
 // Function to handle missed trials asynchronously
 async function handleMissedTrials(MissedTrials, rewardInput) {
@@ -39,9 +46,13 @@ async function handleMissedTrials(MissedTrials, rewardInput) {
   return rewardInput; // Return the updated reward
 }
 
-
 // Function to run all tasks in sequence
 async function runAllTasks() {
+  var timeline = [];
+
+  // Add the dataset name prompt at the beginning of the timeline
+  timeline.push(datasetNameTrial);
+
   console.log("Starting description");
   const orderNumber = await taskDescription();
   console.log("Description finished");
@@ -49,6 +60,13 @@ async function runAllTasks() {
   console.log("Starting first round...");
   let MissedTrials = { TrialNumber: [], Number1: [], Number2: [] };  // Ensure MissedTrials is initialized
   let WholeReward = InitialRewardAmount;
+
+  // Predefined trial numbers for different rounds
+  const trialNumber_fixed_uni = [...Array(eachClassTrialNumber).keys()];
+  const trialNumber_fixed_low_first = [...Array(eachClassTrialNumber).keys()].map(i => i + eachClassTrialNumber);
+  const trialNumber_fixed_high_first = [...Array(eachClassTrialNumber).keys()].map(i => i + 2 * eachClassTrialNumber);
+
+  const trialNumber_mixed = jsPsych.randomization.shuffle([...Array(NumberOfTrials).keys()]);
 
   if (orderNumber == 1) {
     [MissedTrials, WholeReward] = await runTask(jsPsych, trialNumber_fixed_uni, WholeReward);
@@ -98,6 +116,9 @@ async function runAllTasks() {
   WholeReward = await handleMissedTrials(MissedTrials, WholeReward);  // Update WholeReward
 
   console.log("All tasks completed.");
+
+  // Run the timeline
+  jsPsych.run(timeline);
 }
 
 // Start the experiment
